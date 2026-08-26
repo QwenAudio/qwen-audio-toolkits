35828f9dcfc0deda99111e76ad8a641225342148
pengzhendong <275331498@qq.com>
2026-08-26 13:38:13 +0800

    Fix macOS titlebar control alignment for release

--- src/data.ts
+++ src/data.ts
@@ -170,5 +170,5 @@ export const fallbackRuntime: RuntimeStatus = {
   backend: 'Rust + sherpa-onnx',
   device: 'Apple Metal',
   platform: 'Browser preview',
-  version: '0.1.7',
+  version: '0.1.8',
 }
